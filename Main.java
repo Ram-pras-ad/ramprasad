a5c1c92c2e5eedba3f6b5be57f256cb1038d3e68
prasad <50710449+Ram-pras-ad@users.noreply.github.com>
2019-05-24 11:40:15 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	int i,j,n;
+	n=s.nextInt();
+	for(i=0;i<n;i++){
+	    for(j=1;j<=i;j++){
+	        System.out.print(j+" ");
+	}
+	System.out.println();
+	}
+	}
+}
